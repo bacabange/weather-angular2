33380bc02402d13adbe9782e524bc7ed19b67f4c
Stiven Castillo <stivencastillo.90@gmail.com>
2017-03-13 10:45:43 -0500

    Autocomplete form cities

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import 'rxjs/add/operator/startWith';
+import {Observable} from 'rxjs/Rx';
 
 import { CitiesService } from '../../services/cities.service';
 import { City } from '../../models/city';
@@ -16,75 +17,14 @@ import { City } from '../../models/city';
 
 export class SearchInputComponent implements OnInit {
 
-	public cities: Array<any>;
-	stateCtrl: FormControl;
-	filteredStates: any;
-
-	states = [
-		'Alabama',
-		'Alaska',
-		'Arizona',
-		'Arkansas',
-		'California',
-		'Colorado',
-		'Connecticut',
-		'Delaware',
-		'Florida',
-		'Georgia',
-		'Hawaii',
-		'Idaho',
-		'Illinois',
-		'Indiana',
-		'Iowa',
-		'Kansas',
-		'Kentucky',
-		'Louisiana',
-		'Maine',
-		'Maryland',
-		'Massachusetts',
-		'Michigan',
-		'Minnesota',
-		'Mississippi',
-		'Missouri',
-		'Montana',
-		'Nebraska',
-		'Nevada',
-		'New Hampshire',
-		'New Jersey',
-		'New Mexico',
-		'New York',
-		'North Carolina',
-		'North Dakota',
-		'Ohio',
-		'Oklahoma',
-		'Oregon',
-		'Pennsylvania',
-		'Rhode Island',
-		'South Carolina',
-		'South Dakota',
-		'Tennessee',
-		'Texas',
-		'Utah',
-		'Vermont',
-		'Virginia',
-		'Washington',
-		'West Virginia',
-		'Wisconsin',
-		'Wyoming',
-	];
+	public cities: City[];
+	public controlCity;
+	public filteredOptions: Observable<City[]>;
 
 	constructor(
 		private _citiesService: CitiesService
 	) {
-		this.stateCtrl = new FormControl();
-		this.filteredStates = this.stateCtrl.valueChanges
-			.startWith(null)
-			.map(name => this.filterStates(name));
-
-	}
-
-	filterStates(val: string) {
-		return val ? this.states.filter((s) => new RegExp(val, 'gi').test(s)) : this.states;
+		this.controlCity = new FormControl();
 	}
 
 	ngOnInit() {
@@ -92,9 +32,22 @@ export class SearchInputComponent implements OnInit {
 		this._citiesService.getCities().subscribe(
 			result => {
 				this.cities = result;
-				console.log(result);
+
+				this.filteredOptions = this.controlCity.valueChanges
+					.startWith(null)
+					.map(city => city && typeof city === 'object' ? city.name : city)
+					.map(name => name ? this.filter(name) : this.cities.slice(1));
 			}, 
 			error => console.log(error));
+
+	}
+
+	filter(name: string): City[] {
+		return this.cities.filter(option => new RegExp(name, 'gi').test(option.name)); 
+	}
+
+	displayFn(city: City): string {
+		return city ? city.name : null;
 	}
 
 }
